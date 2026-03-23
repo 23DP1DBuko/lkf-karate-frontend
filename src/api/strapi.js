3202f31689aef7114d144b70eff1a5e9342aabe4
hachalupa <bukovskisdeniss@gmail.com>
2026-03-23 23:44:09 +0200

    strapi.js debugging #2

--- src/api/strapi.js
+++ src/api/strapi.js
@@ -3,7 +3,6 @@ import axios from 'axios'
 const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:1337'
 
 console.log('API_URL:', API_URL)
-console.log('ENV:', import.meta.env)
 
 const api = axios.create({
   baseURL: `${API_URL}/api`,
